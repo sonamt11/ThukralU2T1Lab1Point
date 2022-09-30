75c1ab0e823e4ac2884b8f0aee78f4b0a9917db0
ivyl72 <ivyl72@nycstudents.net>
2022-09-30 12:30:15 -0400

    in progress 2

--- src/Point.java
+++ src/Point.java
@@ -23,45 +23,25 @@ public class Point {
     }
 
     //getter for x
-    public int getX() {
-        return x;
-    }
+    public int getX() { return x; }
 
     //setter for x
-    public void setX(int newX) {
-        x = newX;
-    }
+    public void setX(int newX) { x = newX; }
 
     //getter for y
-    public int getY() {
-        return y;
-    }
+    public int getY() { return y; }
 
     //setter for x
-    public void setY(int newY) {
-        y = newY;
-    }
+    public void setY(int newY) { y = newY; }
 
-    public String coordinate() {
-        return "(" + x + ", " + y + ")";
-    }
+    public String coordinate() { return "(" + x + ", " + y + ")"; }
 
     public String quadrant() {
-        if (x > 0 && y > 0) {
-            return "I";
-        }
-        if (x > 0 && y < 0) {
-            return "II";
-        }
-        if (x < 0 && y < 0) {
-            return "III";
-        }
-        if (x < 0 && y > 0) {
-            return "IV";
-        }
-        if (x == 0 && y > 0) {
-            return "on an axis";
-        }
+        if (x > 0 && y > 0) { return "I"; }
+        if (x > 0 && y < 0) { return "II"; }
+        if (x < 0 && y < 0) { return "III"; }
+        if (x < 0 && y > 0) { return "IV"; }
+        if (x == 0 && y > 0) { return "on an axis"; }
         if (x == 0 && y < 0) { return "on an axis"; }
         if (x > 0 && y == 0) { return "on an axis"; }
         if (x < 0 && y == 0) { return "on an axis"; }
