b8c182276c96c596c2be26e2deb6863e7f960d42
ivyl72 <ivyl72@nycstudents.net>
2022-09-30 12:28:07 -0400

    in progress

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,72 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+
+    //instance variables
+    private int x;
+    private int y;
+
+    //constructor - two parameters
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    //constructor - one parameter
+    public Point(int value) {
+        x = value;
+        y = value;
+    }
+
+    //constructor - no parameters
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    //getter for x
+    public int getX() {
+        return x;
+    }
+
+    //setter for x
+    public void setX(int newX) {
+        x = newX;
+    }
+
+    //getter for y
+    public int getY() {
+        return y;
+    }
+
+    //setter for x
+    public void setY(int newY) {
+        y = newY;
+    }
+
+    public String coordinate() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public String quadrant() {
+        if (x > 0 && y > 0) {
+            return "I";
+        }
+        if (x > 0 && y < 0) {
+            return "II";
+        }
+        if (x < 0 && y < 0) {
+            return "III";
+        }
+        if (x < 0 && y > 0) {
+            return "IV";
+        }
+        if (x == 0 && y > 0) {
+            return "on an axis";
+        }
+        if (x == 0 && y < 0) { return "on an axis"; }
+        if (x > 0 && y == 0) { return "on an axis"; }
+        if (x < 0 && y == 0) { return "on an axis"; }
+        if (x == 0 & y == 0); { return "origin" }
+    }
+
+
 }
