d1d957440da66dfb43869d94bd3baefd5aa7cfe9
Sonam <sthukral6331@bths.edu>
2022-10-02 20:18:19 -0400

    final

--- src/BoxRunner.java
+++ src/BoxRunner.java
@@ -1,18 +1,18 @@
-public class BoxRunner {
-    public static void main(String[] args) {
-        Box newBox = new Box(6, 10, 8);
-        double larger = newBox.anySideLongerThan(10); // A
-
-        boolean big = newBox.anySideLongerThan(7.5); // B
-
-        boolean x = newBox.anySideLongerThan(5); // C
-
-        String dim = newBox.printDimensions(); // D
-
-        System.out.println(newBox.printDimensions()); // E
-
-        newBox.printDimensions(); // F
-
-        double volume = newBox.volume(21.8); // G
-    }
-}
+//public class BoxRunner {
+//    public static void main(String[] args) {
+//        Box newBox = new Box(6, 10, 8);
+//        double larger = newBox.anySideLongerThan(10); // A
+//
+//        boolean big = newBox.anySideLongerThan(7.5); // B
+//
+//        boolean x = newBox.anySideLongerThan(5); // C
+//
+//        String dim = newBox.printDimensions(); // D
+//
+//        System.out.println(newBox.printDimensions()); // E
+//
+//        newBox.printDimensions(); // F
+//
+//        double volume = newBox.volume(21.8); // G
+//    }
+//}
--- src/Point.java
+++ src/Point.java
@@ -38,14 +38,14 @@ public class Point {
 
     public String quadrant() {
         if (x > 0 && y > 0) { return "I"; }
-        if (x > 0 && y < 0) { return "II"; }
+        if (x > 0 && y < 0) { return "IV"; }
         if (x < 0 && y < 0) { return "III"; }
-        if (x < 0 && y > 0) { return "IV"; }
+        if (x < 0 && y > 0) { return "II"; }
         if (x == 0 && y > 0) { return "on an axis"; }
         if (x == 0 && y < 0) { return "on an axis"; }
         if (x > 0 && y == 0) { return "on an axis"; }
         if (x < 0 && y == 0) { return "on an axis"; }
-        if (x == 0 & y == 0); { return "origin" }
+        if (x == 0 & y == 0); { return "origin"; }
     }
 
 
